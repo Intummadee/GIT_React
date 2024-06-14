405cea4bd317ec65c0c7bc628b2e6c7b95d7d08b
Intummadee <kataroja1@gmail.com>
2024-06-14 22:21:40 +0700

    init

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
-# GIT_React
- 
 
-คำสั่งรัน
-```
-npx create-react-app myapp
-cd myapp
+
+
+
+
+# คำสั่งรัน
 
 ```
-`
-วิธีรัน
-```
-cd myapp
-npm start
+npm create vite@latest ./ -- --template react
+npm install -D tailwindcss
+npx tailwindcss init
+npm install --legacy-peer-deps @react-three/fiber
+npm install --legacy-peer-deps @react-three/drei
+npm install --legacy-peer-deps maath
+npm install --legacy-peer-deps react-tilt
+npm install --legacy-peer-deps react-vertical-timeline-component
+npm install --legacy-peer-deps @emailjs/browser
+npm install --legacy-peer-deps framer-motion
+npm install --legacy-peer-deps react-router-dom
+npm install --legacy-peer-deps -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
 ```
 
+
+# วิธีรัน
+```
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      // 👇🏼 https://tailwindcss.com/docs/background-color สามารถเรียกใช้สีได้ด้วยการเติมใน class เลยว่า `bg-primary`
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      // 👇🏼 https://tailwindcss.com/docs/background-image สามารถเรียกใช้สีได้ด้วยการเติมใน class เลยว่า `bg-hero-pattern`
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
